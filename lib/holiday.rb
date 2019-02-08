3c554926fbf542c708812cd9617f7bb298e09016
Allen F Partlow <allenfp@gmail.com>
2019-02-08 05:11:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
         holiday = holiday.to_s.split("_")
         puts "  #{holiday.collect {|h| h.capitalize}.join(" ")}: #{items.join(", ")}"
       end
-  end 
+  end
 
 
 def all_holidays_with_bbq(holiday_hash)
