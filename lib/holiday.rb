eac633ab667f40e1e1e06177bc6953eefc189f4d
Allen F Partlow <allenfp@gmail.com>
2019-02-08 04:54:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{key.to_s.capitalize}:"
     value.each do |key_, value_|
       z = value_.join(', ').split("_")
-      puts "  #{key_.to_s.capitalize}: #{z.capitalize}"
+      puts "  #{key_.to_s.capitalize}: #{z}"
 
     end
   end
